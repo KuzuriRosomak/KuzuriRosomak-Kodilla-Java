49b3cc21aac88081117d166fcd8b0a44dc7c4943
KuzuriRosomak <nexadorus@gmail.com>
2018-06-18 13:38:16 +0200

    First Commit

--- kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
+++ kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
@@ -1,9 +1,36 @@
 package com.kodilla.testing;
 
+import com.kodilla.testing.calculator.Calculator;
+
 public class TestingMain
 {
 	public static void main(String[] args)
 	{
-		System.out.println("Moduł 6. Wprowadzenie to testowania oprogramowania");
+		Calculator calculatingMachine = new Calculator();
+		
+		int result1 = calculatingMachine.add(30, 25);
+		
+		if(result1 == 55)
+		{
+			System.out.println("Test 1 - adding - result: Positive");
+		}
+		
+		else
+		{
+			System.out.println("Test 1 - adding - result: Negative");
+		}
+		
+		
+		int result2 = calculatingMachine.subtract(100, 40);
+		
+		if(result2 == 60)
+		{
+			System.out.println("Test 2 - subtracting - result: Positive");
+		}
+		
+		else
+		{
+			System.out.println("Test 2 - subtracting - result: Negative");
+		}
 	}
 }
