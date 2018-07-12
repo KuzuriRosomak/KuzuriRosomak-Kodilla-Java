96231f3289cc49c30d07be2eae2c5a27ad88b17e
KuzuriRosomak <nexadorus@gmail.com>
2018-07-12 14:06:25 +0200

    First sending of a code

---
+++ kodilla-stream/src/main/java/com/kodilla/stream/StreamMain.java
@@ -0,0 +1,33 @@
+package com.kodilla.stream;
+
+import com.kodilla.stream.beautifier.PoemBeautifier;
+import com.kodilla.stream.lambda.ExpressionExecutor;
+import com.kodilla.stream.reference.FunctionalCalculator;
+
+public class StreamMain
+{
+	public static void main(String[] args)
+	{
+		ExpressionExecutor expressionExecutor = new ExpressionExecutor();
+		
+		System.out.println("Calculating expressions with lambdas");
+		expressionExecutor.executeExpression(10, 5, (a, b) -> a + b);
+		expressionExecutor.executeExpression(10, 5, (a, b) -> a - b);
+		expressionExecutor.executeExpression(10, 5, (a, b) -> a * b);
+		expressionExecutor.executeExpression(10, 5, (a, b) -> a / b);
+		
+		System.out.println("Calculating expressions with method references");
+		expressionExecutor.executeExpression(3, 4, FunctionalCalculator::multiplyAByB);
+		expressionExecutor.executeExpression(3, 4, FunctionalCalculator::addAToB);
+		expressionExecutor.executeExpression(3, 4, FunctionalCalculator::subBFromA);
+		expressionExecutor.executeExpression(3, 4, FunctionalCalculator::divideAByB);
+		
+		System.out.println("\nBeautifying text");
+		PoemBeautifier poemBeautifier = new PoemBeautifier();
+		poemBeautifier.beautify("Bowna", (text) -> "ABC" + text + "XYZ");
+		poemBeautifier.beautify("London", (text) -> text.toUpperCase());
+		poemBeautifier.beautify("Poland", (text) -> new StringBuffer(text).reverse().toString());
+		poemBeautifier.beautify("RAGNAR", (text) -> text.toLowerCase());
+		poemBeautifier.beautify("trader", (text) -> text.replaceFirst("der", "itor"));
+	}
+}
