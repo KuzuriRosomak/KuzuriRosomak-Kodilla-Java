0cf86d85f93da8b7aa73dc6445676f429218be94
KuzuriRosomak <nexadorus@gmail.com>
2018-09-02 14:06:33 +0200

    Method in checkout class has been split in 4 boolean return methods. mainmethod in checkout class is depending now on returns from above mentioned methods.

--- kodilla-good-patterns/src/main/java/com/kodilla/good/patterns/challenges/shop/Checkout.java
+++ kodilla-good-patterns/src/main/java/com/kodilla/good/patterns/challenges/shop/Checkout.java
@@ -5,58 +5,67 @@ import java.util.List;
 
 public class Checkout {
 	
+	public boolean ageValidator(User buyer) {
+		boolean validAge = false;
+		if(!(buyer.isEighteen())) {
+			System.out.println(buyer.getLogin() + " your age is under 18. You can't buy anything hear. " + "Please come back later.");
+		} else {
+			validAge = true;
+		}
+		return validAge;
+	}
+	
+	public boolean hasProductValidator(User seller, Product product, User buyer) {
+		boolean hasProduct = false;
+		if(!(seller.getUserProductMap().containsKey(product.getProductName()))) {
+			System.out.println(buyer.getLogin() + " we don't have " + product.getProductName() + " in our stock.");
+		} else {
+			hasProduct = true;
+		}
+		return hasProduct;
+	}
+	
+	public boolean hasEnoughProduct(User buyer, Product product, ShopCart orderedProduct) {
+		boolean hasEnoughProduct = false;
+		if(!((product.getQuantity()) >= (orderedProduct.getProductQuantity()))) {
+			System.out.println(buyer.getLogin() + " we don't have " + orderedProduct.getProductQuantity() + " " + product.getProductName() + " in our stock." + " Please lower amount to " + product.getQuantity() + ".");
+		} else {
+			hasEnoughProduct = true;
+		}
+		return hasEnoughProduct;
+	}
+	
+	public boolean hasEnoughFunds(User buyer, Product product, ShopCart orderedProduct) {
+		boolean hasEnoughFunds = false;
+		if(!(((product.getPricePerUnit()) * (orderedProduct.getProductQuantity())) <= (buyer.getAccountBalance()))) {
+			System.out.println(buyer.getLogin() + " you have insufficient funds." + "You have " + buyer.getAccountBalance() + " and total price for " + orderedProduct.getProductQuantity() + " " + product.getProductName() + " is " + ((orderedProduct.getProductQuantity()) * product.getPricePerUnit()) + "." + " Please lower amount of product or increase the amount of your account balance");
+		} else {
+			hasEnoughFunds = true;
+		}
+		return hasEnoughFunds;
+	}
+	
 	public boolean checkingOut(Shopping shopping) {
 		boolean isDone = false;
 		for(List<ShopCart> userList : shopping.shoppingProcess()) {
 			for(ShopCart currentUsersElementFromShopCart : userList) {
 				System.out.println("Starting processing your order " + currentUsersElementFromShopCart
 						.getThisUserIsBuying().getLogin());
-				if(!(currentUsersElementFromShopCart.getThisUserIsBuying().isEighteen())) {
-					System.out.println(currentUsersElementFromShopCart.getThisUserIsBuying().getLogin()
-							+ " your age is under 18. You can't buy anything hear. Please come back later.");
-					isDone = false;
+				if((ageValidator(currentUsersElementFromShopCart.getThisUserIsBuying()))
+						&& (hasProductValidator(currentUsersElementFromShopCart.getThisUserIsSelling(),
+						currentUsersElementFromShopCart.getProduct(), currentUsersElementFromShopCart
+								.getThisUserIsBuying()))
+						&& (hasEnoughProduct(currentUsersElementFromShopCart.getThisUserIsBuying(),
+						currentUsersElementFromShopCart.getProduct(), currentUsersElementFromShopCart))
+						&& (hasEnoughFunds(currentUsersElementFromShopCart.getThisUserIsBuying(),
+						currentUsersElementFromShopCart.getProduct(), currentUsersElementFromShopCart))) {
+					isDone = true;
+					System.out.println("Your order of: " + currentUsersElementFromShopCart.getProductQuantity() + " " + currentUsersElementFromShopCart.getProduct().getProductName() + " have been" + " processed successfully. Your order soon will be in the way.");
+					ProductDatabase.getInstance().changingQuantityInMapMechanism(currentUsersElementFromShopCart.getProduct(), currentUsersElementFromShopCart.getProductQuantity());
+					currentUsersElementFromShopCart.setTimeOfOrder(LocalDateTime.now());
+					ArchiveService.getInstance().addingToOrderArchive(currentUsersElementFromShopCart);
 				} else {
-					if(!(currentUsersElementFromShopCart.getThisUserIsSelling().getUserProductMap()
-							.containsKey(currentUsersElementFromShopCart.getProduct().getProductName()))) {
-						isDone = false;
-						System.out.println(currentUsersElementFromShopCart.getThisUserIsBuying().getLogin() + " we don't have "
-								+ currentUsersElementFromShopCart.getProduct().getProductName()
-								+ " in our stock.");
-					}
-					else {
-						if(!((currentUsersElementFromShopCart.getProduct().getQuantity()) >= (currentUsersElementFromShopCart
-								.getProductQuantity()))) {
-							isDone = false;
-							System.out.println(currentUsersElementFromShopCart.getThisUserIsBuying().getLogin() +
-									" we don't have " + currentUsersElementFromShopCart.getProductQuantity()
-									+ " " + currentUsersElementFromShopCart.getProduct().
-									getProductName() + " in our stock." + " Please lower amount to "
-									+ currentUsersElementFromShopCart.getProduct().getQuantity() + ".");
-						} else {
-							if(!(((currentUsersElementFromShopCart.getProduct().getPricePerUnit())
-									* (currentUsersElementFromShopCart.getProductQuantity()))
-									<= (currentUsersElementFromShopCart.getThisUserIsBuying().getAccountBalance()))) {
-								isDone = false;
-								System.out.println(currentUsersElementFromShopCart.getThisUserIsBuying().getLogin()
-										+ " you have insufficient funds." + "You have " + currentUsersElementFromShopCart
-										.getThisUserIsBuying().getAccountBalance() + " and total price for "
-										+ currentUsersElementFromShopCart.getProductQuantity() + " "
-										+ currentUsersElementFromShopCart.getProduct().getProductName() + " is "
-										+ ((currentUsersElementFromShopCart.getProductQuantity())
-										* currentUsersElementFromShopCart.getProduct().getPricePerUnit()) + "."
-										+ " Please lower amount of product or increase the amount of your account balance");
-							} else {
-								isDone = true;
-								System.out.println("Your order of: " + currentUsersElementFromShopCart.getProductQuantity()
-										+ " " + currentUsersElementFromShopCart.getProduct().getProductName() + " have been" +
-										" processed successfully. Your order soon will be in the way.");
-								ProductDatabase.getInstance().changingQuantityInMapMechanism(currentUsersElementFromShopCart
-										.getProduct(), currentUsersElementFromShopCart.getProductQuantity());
-								currentUsersElementFromShopCart.setTimeOfOrder(LocalDateTime.now());
-								ArchiveService.getInstance().addingToOrderArchive(currentUsersElementFromShopCart);
-							}
-						}
-					}
+					isDone = false;
 				}
 			}
 		}
